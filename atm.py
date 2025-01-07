26be94f5424f251a3f22b0bc34c9e00c08b8409c
LucasKim4321 <xowls0131@naver.com>
2025-01-07 19:57:27 +0900

    🚀 완벽한 While문을 사용한 나만의 ATM system!!
    기존에 만들었던 코드에 while문 적용!!  :b

--- atm.py
+++ atm.py
@@ -8,70 +8,41 @@ print("이전 영수증 리스트:",receipts)
 
 balance = 7000
 
+# 종료 할 때까지 무한 반복
+while True:
 
-# while True:
-#     service_num = input("사용하실 기능을 선택해주세요 (1:입금, 2:출금, 3:영수증 보기, 4:종료)")
-#     if service_num == '4':
-#         print(f"현재 잔액은 {balance}원 입니다!")
-#         break
-#     if service_num == '1' or '2':
-#         amount = int(input('금액을 입력하세요. :b').strip())  #strip() 없어도 문제없긴 함.
-#         초과_여부 = False
+    # 서비스 코드 입력
+    service_num = input("사용하실 기능을 선택해주세요 (1:입금, 2:출금, 3:영수증 보기, 4:종료)")
 
-#         if service_num == '2':
-#             if amount > balance :
-#                 amount = balance
-#                 초과_여부 = True
-#             amount = -amount
-#     balance += amount
-#     거래_정보 = (amount, balance)
-#     receipts.append(거래_정보)
-#     print("거래 정보:",거래_정보)
-#     print("영수증 리스트:",receipts)
+    # 입출금
+    if service_num == '1' or  service_num == '2':
+        amount = int(input('금액을 입력하세요. :b').strip())  #strip() 없어도 문제없긴 함.
+
+        초과_여부 = False
+
+        if service_num == '2':
+            if amount > balance :
+                amount = balance
+                초과_여부 = True
+            amount = -amount
         
+        balance += amount
+        거래_정보 = (amount, balance)
+        receipts.append(거래_정보)
+        
+        if(초과_여부 == True):
+            print(f"************************\n영수증\n****\n출금 가능한 금액을 초과하여 현재 잔액 만큼만 출금합니다!\n****\n{'입금' if service_num == '1' else '출금'}: {abs(amount)}원. \n현재 잔액은 {balance}원입니다.\n****\n************************")
+        else:
+            print(f"************************\n영수증\n****\n{'입금' if service_num == '1' else '출금'}: {abs(amount)}원. \n현재 잔액은 {balance}원입니다.\n****\n************************")
 
+    # 영수증 출력
+    if service_num == '3':
+        print('현재 잔액:', balance)
+        print("모든 거래 내역")
+        for i in range(len(receipts)):
+            print(f"{'입금' if receipts[i][0] > 0 else '출금'} : {abs(receipts[i][0])}, 잔액 : {receipts[i][1]}")
 
-# 입출금_여부 = input('입금 또는 출금을 입력하세요').strip()  #strip() 양쪽 공백 제거
-
-
-# if(초과_여부 == True):
-#     print(f'************************\n영수증\n****\n출금 가능한 금액을 초과하여 현재 잔액 만큼만 출금합니다!\n****\n{입출금_여부}: {abs(amount)}원. \n현재 잔액은 {balance}원입니다.\n****\n************************')
-
-# else:
-#     print(f'************************\n영수증\n****\n{입출금_여부}: {abs(amount)}원. \n현재 잔액은 {balance}원입니다.\n****\n************************')
-
-
-"""
-
-
-# 영수증 구조 [ (입금 or 출금 금액 , 현재잔액), (이하 동일) ... ]
-
-receipts = [ (2000, 3000), (-1000, 2000), (5000, 7000) ]
-print("이전 영수증 리스트:",receipts)
-
-balance = 7000
-
-입출금_여부 = input('입금 또는 출금을 입력하세요').strip()  #strip() 양쪽 공백 제거
-amount = int(input('금액을 입력하세요. :b').strip())  #strip() 없어도 문제없긴 함.
-초과_여부 = False
-
-if(입출금_여부 == '출금'):
-    if(amount > balance):
-        amount = balance
-        초과_여부 = True
-    amount = -amount
-
-balance += amount
-거래_정보 = (amount, balance)
-receipts.append(거래_정보)
-print("거래 정보:",거래_정보)
-print("영수증 리스트:",receipts)
-
-if(초과_여부 == True):
-    print(f'************************\n영수증\n****\n출금 가능한 금액을 초과하여 현재 잔액 만큼만 출금합니다!\n****\n{입출금_여부}: {abs(amount)}원. \n현재 잔액은 {balance}원입니다.\n****\n************************')
-
-else:
-    print(f'************************\n영수증\n****\n{입출금_여부}: {abs(amount)}원. \n현재 잔액은 {balance}원입니다.\n****\n************************')
-
-
-"""
+    # 종료
+    if service_num == '4':
+        print(f"현재 잔액은 {balance}원 입니다!")
+        break

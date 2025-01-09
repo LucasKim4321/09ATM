cc9ed9c1057fb78991b47e50ed9127493cd71508
LucasKim4321 <xowls0131@naver.com>
2025-01-09 17:12:52 +0900

    🐛 입력 제한 업데이트
    숫자만 입력 받는 제한 사항 수정

--- atm.py
+++ atm.py
@@ -13,7 +13,7 @@ while True:
 
     # 서비스 코드 입력
     service_num = input("사용하실 기능을 선택해주세요 (1:입금, 2:출금, 3:영수증 보기, 4:종료)").strip()
-    # 숫자 입력시에만 형변환, 덤으로 -도 입력 안됨 :b
+    # 숫자 입력만 가능
     if not(service_num.isdigit()):
         print("숫자만 입력해주세요!")
         continue
@@ -23,11 +23,11 @@ while True:
         while True:
             amount = input(f"{'입금' if service_num == '1' else '출금'}하실 금액을 입력하세요. :b  ").strip()  #strip() 없어도 문제없긴 함.
             # 숫자 입력시에만 형변환, 덤으로 -도 입력 안됨 :b
-            if not(amount.isdigit()):
+            if amount.isdigit(): #문자형 숫자일 경우만.
+                amount = int(amount)
+            else:
                 print("숫자만 입력해주세요!")
                 continue
-            else:
-                amount = int(amount)
 
             초과_여부 = False
 
